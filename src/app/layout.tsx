4f67b6c318cac72c4575567ff3b11f007045aee3
ADESINA AYOMIDE <adesinayomide2@gmail.com>
2026-03-18 16:13:26 +0100

    Rollback to web analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from 'next';
 import { Analytics } from '@vercel/analytics/next';
-import { SpeedInsights } from '@vercel/speed-insights/next';
 import './globals.css';
 
 export const metadata: Metadata = {
@@ -21,7 +20,6 @@ export default function RootLayout({
       <body className="font-body antialiased">
         {children}
         <Analytics />
-        <SpeedInsights />
       </body>
     </html>
   );
