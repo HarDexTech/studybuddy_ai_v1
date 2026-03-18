0367f6aac03accbb6a2a92b8564c8c6334114a40
ADESINA AYOMIDE <adesinayomide2@gmail.com>
2026-03-18 15:19:16 +0100

    FIXED BUILD ISSUES

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  serverExternalPackages: [
+    'express',
+    '@genkit-ai/core',
+    'genkit',
+    'import-in-the-middle',
+    'require-in-the-middle',
+  ],
   images: {
     remotePatterns: [
       {
